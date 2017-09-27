5c5651f767b8572c7958445aee6c7a50ba7ffab0
Student P_08 <studentp_08@Student-P-08.local>
2017-09-27 14:40:21 +0530

    Initial Commit

---
+++ day16_iOS_whereIAm_locationApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  day16_iOS_whereIAm_locationApp
+//
+//  Created by Student P_08 on 27/09/17.
+//  Copyright © 2017 pranjal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ day16_iOS_whereIAm_locationApp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  day16_iOS_whereIAm_locationApp
+//
+//  Created by Student P_08 on 27/09/17.
+//  Copyright © 2017 pranjal. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
